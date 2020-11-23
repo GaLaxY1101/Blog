5e890a9957440f99a72ffbb7aa44e4265631eef7
GaLaxY1101 <70031590+GaLaxY1101@users.noreply.github.com>
2020-11-23 13:25:32 +0200

    Begin

--- account/forms.py
+++ account/forms.py
@@ -39,3 +39,28 @@ class AccountAuthenticationForm(forms.ModelForm):
 		password = self.cleaned_data['password']
 		if not authenticate(email = email, password = password):
 			raise forms.ValidationError('Invalid login')
+
+class AccountUpdateForm(forms.ModelForm):
+
+	class Meta:
+		model = Account
+		fields = ('email','username')
+	
+	def clean_email(self):
+		if self.is_valid():
+			email = self.cleanned_data['email']
+			try:
+				account = Account.objects.exclude(pk = self.instance.pk).get(email = email)
+			except Account.DoesNotExist:
+				return email
+			raise ("Email %s is already used." % email)
+
+	def clean_username(self):
+		if self.is_valid():
+			username = self.cleanned_data['username']
+			try:
+				account = Account.objects.exclude(pk = self.instance.pk).get(username = username)
+			except Account.DoesNotExist:
+				return username
+			raise ("Username %s is already used." % username)		
+
