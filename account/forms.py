e666c0c48515eda40242df0cac343e67b37f2294
GaLaxY1101 <korniykhik3@gmail.com>
2020-11-24 17:18:52 +0200

    Completed account view

--- account/forms.py
+++ account/forms.py
@@ -27,8 +27,8 @@ class AccountAuthenticationForm(forms.ModelForm):
 	email = forms.CharField(max_length=60, widget = 
 	forms.TextInput(attrs={'type':'text', 'id':'email', 'class':'form-control', 'name':'email'}))
 	
-	password = forms.CharField(max_length=60, widget = 
-	forms.TextInput(attrs={'type':'password', 'id':'password', 'class':'form-control', 'name':'password'}))
+	username = forms.CharField(max_length=60, widget = 
+	forms.TextInput(attrs={'type':'text', 'id':'user_name', 'class':'form-control', 'name':'username'}))
 
 	class Meta:
 		model = Account
@@ -40,7 +40,13 @@ class AccountAuthenticationForm(forms.ModelForm):
 		if not authenticate(email = email, password = password):
 			raise forms.ValidationError('Invalid login')
 
+
 class AccountUpdateForm(forms.ModelForm):
+	email = forms.CharField(max_length=60, widget = 
+	forms.TextInput(attrs={'type':'text', 'id':'email', 'class':'form-control', 'name':'email'}))
+
+	username = forms.CharField(max_length=60, widget = 
+	forms.TextInput(attrs={'type':'text', 'id':'user_name', 'class':'form-control', 'name':'username'}))
 
 	class Meta:
 		model = Account
@@ -48,19 +54,19 @@ class AccountUpdateForm(forms.ModelForm):
 	
 	def clean_email(self):
 		if self.is_valid():
-			email = self.cleanned_data['email']
+			email = self.cleaned_data['email']
 			try:
 				account = Account.objects.exclude(pk = self.instance.pk).get(email = email)
 			except Account.DoesNotExist:
 				return email
-			raise ("Email %s is already used." % email)
+			raise forms.ValidationError("Email %s is already used." % email)
 
 	def clean_username(self):
 		if self.is_valid():
-			username = self.cleanned_data['username']
+			username = self.cleaned_data['username']
 			try:
 				account = Account.objects.exclude(pk = self.instance.pk).get(username = username)
 			except Account.DoesNotExist:
 				return username
-			raise ("Username %s is already used." % username)		
+			raise forms.ValidationError("Username %s is already used." % username)		
 
